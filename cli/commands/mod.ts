2d244da66c0582350c6733c7dff772c8b4c11e1c
Alisue <lambdalisue@gmail.com>
2025-11-27 10:35:05 +0900

    fix: Remove unnecessary type exports

--- cli/commands/mod.ts
+++ cli/commands/mod.ts
@@ -7,7 +7,3 @@
 export { initCommand } from "./init.ts";
 export { listCommand } from "./list.ts";
 export { runCommand } from "./run.ts";
-
-export type { InitCommandOptions } from "./init.ts";
-export type { ListCommandOptions } from "./list.ts";
-export type { RunCommandOptions } from "./run.ts";
